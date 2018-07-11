c921538f94db99d8ed53ac60197eb614dc37779d
John Doe <tonyennis@yahoo.com>
2018-07-11 22:55:57 +0100

    .

--- src/com/iot/pipeline.groovy
+++ src/com/iot/pipeline.groovy
@@ -14,11 +14,7 @@ def test(name) {
 }
 
 def deploy(env,app) {
-  aws cloudformation create-stack \
-  --stack-name ${env}_{app)
-  --parameters \
-    ParameterKey=Env,ParameterValue=${env}
-  ...(rest of the params go here)
+  println "Deploying to ${env}_{app} "
 }
 
 // AimTheory have a recommendation and explanation about this here
